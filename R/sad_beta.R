335c90698bf2fb3198cf80f90f6019b25ba24a5c
Ross Whippo <rosspitality@gmail.com>
2014-12-23 15:25:11 -0800

    Removed commented out functions. Converting this into RAD only function with lm() output. Will rename sad_rad(), sad_beta() will become second exported function.

--- R/sad_beta.R
+++ R/sad_beta.R
@@ -1,31 +1,23 @@
-#' RAD and Beta Diversity Null Models
+#' RAD Null Models
 #' 
 #' This function randomly populates community matrices from various species abundance distributions (SADs),
-#' based on input parameters chosen by the user. Possbile outputs include rank abundance distribution (RAD)
-#' plots, beta diversity plots, RAD quartiles, and a .csv of all permuted communities. 
+#' based on input parameters chosen by the user. 
 #' 
 #' @param spp_count number of species to use in simulation
 #' @param Nsamp number of individuals to sample in each plot 
 #' @param Nplot number of 'plots' sampled per round
 #' @param Nsite number of 'sites' to sample, essentially number of simulations to run
 #' @param dist distribution to use for calculating RAD
-#' @param method beta measurement used
 #' @param gamma_step integer for stepwise increase of gamma with each progressive site sampled
-#' @param radplot Logical. If TRUE RAD plot will be generated
-#' @param betaplot Logical. If TRUE beta plot will be generated
-#' @param csvoutput Logical. If TRUE the entire data set is exported into .csv in working directory
-#' @return describe output of function
+#' @return Uses linear model to fit line to rank abundance distribution, plots and provides slope. 
 #' @author Ross Whippo
 #' @examples 
 #' #description of examples here
 #' #actual R code to execute examples
 #' @export 
 #' @importFrom VGAM pzipf
-#' @importFrom gtools mixedsort
-#' @importFrom vegan vegdist 
 #' 
-sad_beta <- function(spp_count, Nsamp, Nplot = 16,  Nsite = 9, dist = "qlnorm", #method = 'bray', 
-                     gamma_step = 0) #radplot = TRUE, betaplot = TRUE, csvoutput = FALSE) 
+sad_beta <- function(spp_count, Nsamp, Nplot = 16,  Nsite = 9, dist = "qlnorm", gamma_step = 0)
 {
   DISTS <- c("qlnorm", "qgeom", "qzipf") 
   dist <- pmatch(dist, DISTS)
@@ -66,83 +58,20 @@ sad_beta <- function(spp_count, Nsamp, Nplot = 16,  Nsite = 9, dist = "qlnorm", 
     }
     comm.mat <- comm.mat[,sort.list(as.numeric(colnames(comm.mat)))] #reorders columns by species ID
     comm.mat[is.na(comm.mat)] <- 0 #replace NAs with 0
-    #newcols <- seq(1,(spp_count + (gamma_step * Nsite)), 1) #vector to align column names between matrices
-    #newcols <- as.character(newcols) #make numeric vector into characters
-    #total.comm <- merge(total.comm, comm.mat, by = newcols, all = TRUE)  #add all draws to master matrix
     comm.cols <- colSums(comm.mat) #sum species totals for site[i]
     comm.cols <- t(as.matrix(comm.cols)) #turn sums into matrix
     rad.totals[i,] <- comm.cols #add summed species abund totals to site matrix
-    
-    #METH <- c("bray", "whittaker", "additive", "altGower")
-    #meth <- pmatch(method, METH)
-    #inm <- METH[meth]
-    #if (is.na(method))
-    #  stop("invalid method")
-    
-    #if (meth == 1){
-    #  beta.dist <- vegdist(comm.mat, method = "bray") #calculates bray curtis pairwise distances
-    #  beta.quan <- quantile(beta.dist) #creates list of quartiles for sample
-    #  beta.mat[i,] <- beta.quan
-    #}
-    
-    #if (meth == 2){
-    #  comm.mat[comm.mat > 0] <- 1
-    #  beta.dist <- apply(comm.mat, 1, sum)
-    #  beta.mean <- mean(beta.dist)  
-    #  beta.value <- (spp_count + (gamma_step*i)/beta.mean)
-    #  beta.mat[i,1] <- beta.value
-    #}
-    
-    #if (meth == 3){
-    #  comm.mat[comm.mat > 0] <- 1
-    #  beta.dist <- apply(comm.mat, 1, sum)
-    #  beta.mean <- mean(beta.dist)  
-    #  beta.value <- (spp_count + (gamma_step*i) - beta.mean)
-    #  beta.mat[i,1] <- beta.value
-    #}
-    
-    #if (meth == 4){
-    #  beta.dist <- vegdist(comm.mat, method = "altGower") #calculates altGower pairwise distances
-    #  beta.quan <- quantile(beta.dist) #creates list of quartiles for sample
-    #  beta.mat[i,] <- beta.quan
-    #}
   }
-  
   colnames(rad.totals) <- c(as.numeric(1:(spp_count + (gamma_step * Nsite)))) #rename matrix columns for ease of reading
   rad.quantiles <- apply(rad.totals, 2, quantile)
   rad.means <- apply(rad.totals, 2, mean)
   new.rad <- rbind(rad.means, sp)
-  plot(new.rad[1, ] ~ new.rad[2,])
-  rad.lm <- lm(new.rad[1, ] ~ new.rad[2,])
+  plot(log(new.rad[1, ]) ~ new.rad[2,], xlab = "spp", ylab = "log abundance")
+  rad.lm <- lm(log(new.rad[1, ]) ~ new.rad[2,])
+  lm.summ <- summary(rad.lm)
   abline(rad.lm)
   
-  
-  #if (radplot == TRUE){
-  #  rad.means <- apply(rad.totals, 2, quantile)
-  #  plot(1:(spp_count + (gamma_step * Nsite)), rad.means[4,], type = 'n', xlab = "Rank", ylab = "Mean Abundance", main = dQuote(ind))
-  #  lines(1:(spp_count + (gamma_step * Nsite)), rad.means[3,], lwd = 2)
-  #  lines(1:(spp_count + (gamma_step * Nsite)), rad.means[4,], lty = 2)
-  #  lines(1:(spp_count + (gamma_step * Nsite)), rad.means[2,], lty = 2)
-  #  }
-  
-  #if (betaplot == TRUE){
-  #  if (meth %in% c(1, 4)){
-  #    beta.result <- t(beta.mat)
-  #    colnames(beta.result) <- 1:Nsite
-  #    rownames(beta.result) <- rownames(rad.means)
-  #    boxplot(t(beta.mat), xlab = "Site(simulation) #", ylab = c(dQuote(inm),"distance to centroid"))
-  #    } 
-  
-#    if (meth %in% c(2,3)){
-#      beta.result <- as.matrix(beta.mat[,1])
-#      plot(beta.result, cex = 2, type = 'p', pch = 15, xlab = "Site(simulation) #", ylab = c(dQuote(inm),"beta"))
-#      }
-#  }
-#  
-#  if (csvoutput == TRUE)
-#  write.csv(total.comm, file = "TotalComm.csv")
-#  
-  results <- c(rad.means, rad.quantiles)
-  return(rad.means)
+  results <- lm.summ
+  return(results)
   
 } 
